f56f22bbe75c604345dd61a2950dfefbb5765936
Big-Plato <luisoctavius.sc@gmail.com>
2025-02-16 15:29:20 -0300

    change promises to async/await

--- app.js
+++ app.js
@@ -17,19 +17,13 @@ function createGifs(src) {
 }
 
 async function fetchCatGif(search) {
-  fetch(
+  const response = await fetch(
     `https://api.giphy.com/v1/gifs/search?api_key=1cbzbJFmH5vZwyM3T1Ny0KepjJ1f7O3i&q=${search}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`,
     { mode: "cors" }
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (response) {
-      console.log(response);
-      for (let i = 0; i < response.data.length; i++) {
-        const src = response.data[i].images.original.url;
-        createGifs(src);
-      }
-    })
-    .catch(function (err) {});
+  );
+  const gifData = await response.json();
+  for (let i = 0; i < gifData.data.length; i++) {
+    const src = gifData.data[i].images.original.url;
+    createGifs(src);
+  }
 }
