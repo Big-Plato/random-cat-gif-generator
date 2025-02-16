ea22da9a866391f89455028baf9492eaae734959
Big-Plato <luisoctavius.sc@gmail.com>
2025-02-16 10:02:11 -0300

    add: search functionality

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
-const img = document.querySelector("img");
-fetchCatGif();
-const button = document.querySelector("button");
-button.addEventListener("click", fetchCatGif);
+const gifImg = document.querySelector("#gif");
+const searchInput = document.querySelector("#search");
+const searchImg = document.querySelector("#search-img");
+const imgSection = document.querySelector("section-img");
+const gifsArray = [];
 
-function fetchCatGif() {
+fetchCatGif();
+
+searchImg.addEventListener("click", () => {
+  console.log("Clicked");
+})
+
+function createGifs(src) {
+  const gif = document.createElement("img");
+  gif.src = src;
+  imgSection.appendChild(gif);
+}
+
+function fetchCatGif(search) {
   fetch(
-    "https://api.giphy.com/v1/gifs/translate?api_key=1cbzbJFmH5vZwyM3T1Ny0KepjJ1f7O3i&s=cats",
+    `https://api.giphy.com/v1/gifs/search?api_key=1cbzbJFmH5vZwyM3T1Ny0KepjJ1f7O3i&q=${search}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`,
     { mode: "cors" }
   )
     .then(function (response) {
       return response.json();
     })
     .then(function (response) {
-      img.src = response.data.images.original.url;
+      console.log(response)
+        // img.src = response.data.images.original.url;
     });
 }
 
