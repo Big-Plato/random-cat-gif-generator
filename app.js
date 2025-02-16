5becdb30a9fbe0833517b090d84633dd2594a0d2
Big-Plato <luisoctavius.sc@gmail.com>
2025-02-16 15:43:56 -0300

    add: proper try/catch block to deal with errors

--- app.js
+++ app.js
@@ -2,10 +2,8 @@ const gifImg = document.querySelector("#gif");
 const searchInput = document.querySelector("#search");
 const searchImg = document.querySelector("#search-img");
 const imgSection = document.querySelector("#img-section");
-const gifsArray = [];
 
 searchImg.addEventListener("click", () => {
-  console.log("Clicked");
   fetchCatGif(searchInput.value);
   imgSection.innerHTML = "";
 });
@@ -17,13 +15,23 @@ function createGifs(src) {
 }
 
 async function fetchCatGif(search) {
-  const response = await fetch(
-    `https://api.giphy.com/v1/gifs/search?api_key=1cbzbJFmH5vZwyM3T1Ny0KepjJ1f7O3i&q=${search}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`,
-    { mode: "cors" }
-  );
-  const gifData = await response.json();
-  for (let i = 0; i < gifData.data.length; i++) {
-    const src = gifData.data[i].images.original.url;
-    createGifs(src);
+  try {
+    const response = await fetch(
+      `https://api.giphy.com/v1/gifs/search?api_key=1cbzbJFmH5vZwyM3T1Ny0KepjJ1f7O3i&q=${search}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`,
+      { mode: "cors" }
+    );
+    const gifData = await response.json();
+    // Gipyhy API returns an empty array if the searched term not returns anything
+    if (gifData.data.length === 0) {
+      throw new Error(`${search} is invalid!`)
+    } else {
+      for (let i = 0; i < gifData.data.length; i++) {
+        const src = gifData.data[i].images.original.url;
+        createGifs(src);
+      }
+    }
+    
+  } catch (err) {
+    alert(err);
   }
 }
